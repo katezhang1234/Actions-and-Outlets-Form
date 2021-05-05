a1b6011bbfe4c5807cc13b23894e3450e1defffa
Kate Zhang <iamkate1226@gmail.com>
2021-05-04 23:15:09 -0700

    Initial Commit

---
+++ Actions and Outlets Form/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Actions and Outlets Form
+//
+//  Created by Kate Zhang on 5/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
